a8a5c06dcc3eab7374292967e73bc41e815b1eb4
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2021-12-04 20:23:27 +0900

    firestore setting

---
+++ lib/data/user_model.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+
+class UserModel {
+  late String userKey;
+  late String phoneNumber;
+  late String address;
+  late num lat;
+  late num lon;
+  late GeoFirePoint geoFirePoint;
+  late DateTime createdDate;
+  DocumentReference? reference;
+
+  UserModel({
+    required this.userKey,
+    required this.phoneNumber,
+    required this.address,
+    required this.lat,
+    required this.lon,
+    required this.geoFirePoint,
+    required this.createdDate,
+    required this.reference,
+  });
+
+  UserModel.fromJson(Map<String, dynamic> json, this.userKey, this.reference) {
+    userKey = json["userKey"];
+    phoneNumber = json["phoneNumber"];
+    address = json["address"];
+    lat = json["lat"];
+    lon = json["lon"];
+    geoFirePoint = GeoFirePoint((json["geoFirePoint"]["geopoint"]).latitude,
+        (json["geoFirePoint"]["geopoint"]).longitude);
+    createdDate = json["createdDate"] == null
+        ? DateTime.now().toUtc()
+        : (json["createdDate"] as Timestamp).toDate();
+    reference = json["reference"];
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map["userKey"] = userKey;
+    map["phoneNumber"] = phoneNumber;
+    map["address"] = address;
+    map["lat"] = lat;
+    map["lon"] = lon;
+    map["geoFirePoint"] = geoFirePoint;
+    map["createdDate"] = createdDate;
+    map["reference"] = reference;
+    return map;
+  }
+}
